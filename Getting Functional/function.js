b08f5d5f00aba686b30ba5cf2cff2fb635b560f7
ejbesana04 <ej04besana@gmail.com>
2025-03-20 15:38:19 +0800

    battleship v2 & functions

---
+++ Getting Functional/function.js
@@ -0,0 +1,44 @@
+var dogName = "Uno";
+var dogWeight = 19;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+var dogName = "Dos";
+var dogWeight = 24;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+var dogName = "Tres";
+var dogWeight = 28;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+var dogName = "Quatro";
+var dogWeight = 19;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + " says woof woof");
+    }
+}
+
+bark("Uno", 19);
+bark("Dos", 23);
+
