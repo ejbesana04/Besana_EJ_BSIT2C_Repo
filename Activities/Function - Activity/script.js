3f2cb4c60e731bd4785551584b40e3db139f7c44
ejbesana04 <ej04besana@gmail.com>
2025-03-24 15:13:26 +0800

    Function Activity 1

---
+++ Activities/Function - Activity/script.js
@@ -0,0 +1,13 @@
+function whatShallIwear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacker");
+    } else if (temp < 70) {
+        console.log("Wear a sweater");
+    } else {
+        console.log("Wear a t-shirt");
+    }
+}
+
+whatShallIwear(50);
+whatShallIwear(60);
+whatShallIwear(80);
